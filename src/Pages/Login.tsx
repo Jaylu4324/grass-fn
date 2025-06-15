621f1df98c49c91ccdfe0f53f0aea3b511ce0a4e
krunal-29 <137616625+krunal-29@users.noreply.github.com>
2025-06-15 14:04:50 +0530

    hello

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -131,7 +131,7 @@ const Login = () => {
                 {/* Login button */}
                 <Button className="w-full py-5 bg-gradient-to-r from-green-600 to-emerald-600 hover:from-green-700 hover:to-emerald-700 text-white font-semibold rounded-lg shadow-lg hover:shadow-xl transform hover:scale-[1.02] transition-all duration-300 text-lg">
                   <span className="flex items-center justify-center space-x-2">
-                    <span>Harsh</span>
+                    <span>regeste</span>
                     <Sparkles className="h-5 w-5 animate-pulse" />
                   </span>
                 </Button>
